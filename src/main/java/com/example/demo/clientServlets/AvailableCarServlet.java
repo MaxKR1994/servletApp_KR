ffe6ad561bbffd65dcceb2356420b911267e2e2e
MaxKR1994 <98046645+MaxKR1994@users.noreply.github.com>
2022-08-23 16:10:30 +0300

    Create new servlet

--- src/main/java/com/example/demo/clientServlets/AvailableCarServlet.java
+++ src/main/java/com/example/demo/clientServlets/AvailableCarServlet.java
@@ -1,4 +1,7 @@
-package com.example.demo;
+package com.example.demo.clientServlets;
+
+import com.example.demo.Car;
+import com.example.demo.CarRepository;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
